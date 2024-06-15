3621a76484b7e8e7126eaf9794302017f5fdb4ca
Christian Adler <chris.adler@gmx.de>
2024-06-15 05:36:54 +0200

    use lerp for scale to grow smoothly

--- main.mjs
+++ main.mjs
@@ -1,4 +1,5 @@
 import {RecamanSequence} from "./recamansequence.mjs";
+import {lerp} from "./utils.mjs";
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
@@ -23,6 +24,8 @@ const sequence = new RecamanSequence();
 sequence.step();
 
 let percent = 0;
+let percentIncrement = 0.01;
+let prevMax = 1;
 
 const update = () => {
 
@@ -30,7 +33,7 @@ const update = () => {
 
   ctx.save();
 
-  const scale = worldWidth / (2 + sequence.max - sequence.min);
+  const scale = worldWidth / (2 + lerp(prevMax, sequence.max, percent) - sequence.min);
   ctx.translate(scale, worldHeight2);
   ctx.scale(scale, scale);
   ctx.lineWidth = 1 / scale;
@@ -39,11 +42,13 @@ const update = () => {
   ctx.restore();
   updateWorldSettings();
 
-  percent += 0.01;
+  percent += percentIncrement;
   if (percent > 1) {
     percent = 0;
-
+    prevMax = sequence.max;
     sequence.step();
+    if (percentIncrement < 0.1)
+      percentIncrement += 0.01;
   }
 
   requestAnimationFrame(update);
